f938474803378126aaa53c2486e87e11ee5b8576
Natã Melo <nata.melo@vtex.com.br>
2022-06-02 09:19:37 -0300

    cypree 10

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,4 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
+Cypress.config({
   retries: 0,
   video: true,
   trashAssetsBeforeRuns: true,
@@ -8,11 +6,6 @@ export default defineConfig({
   viewportWidth: 1000,
   viewportHeight: 600,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'http://localhost:3000/',
   },
 })
